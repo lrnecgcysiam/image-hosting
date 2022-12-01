440015bafc96c804ff001a1659565b60ff9a1f66
Chris Samuel <encryptable@pm.me>
2022-12-01 12:36:38 +0900

    testing the corresponding tags

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,13 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        {/* <!-- Facebook Meta Tags --> */}
+<meta property="og:url" content="https://lrnecgysiam.com/"></meta>
+<meta property="og:type" content="website"></meta>
+<meta property="og:title" content="lrnecgcysiam"></meta>
+<meta property="og:description" content="A Personal blog on Web, Personal, and cultural Development"></meta>
+<meta property="og:image" content="https://avatars.githubusercontent.com/u/88948670?s=400&u=37177928f43cd41c701284b1eae589f9235e768a&v=4
+    "></meta>
       </Head>
 
       <main>
